82de9c9ad2dbd736a92d90b00a993fda3bc3f240
cyy <115590578@qq.com>
2016-01-13 15:59:00 +0800

    创建MyAdapter类

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,40 @@ package sampleanalasis.zzbk.cn.gittester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-  
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String s = new String();
     }
+
+    public class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
 }
