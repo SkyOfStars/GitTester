baf0e5e19baf588d8f7c2a8d894e2078562fef67
cyy <115590578@qq.com>
2016-01-13 16:13:37 +0800

    创建MyAdapter类

--- MainActivity.java
+++ MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String s = new String();
+        System.out.println("zz");
     }
 
     public class MyAdapter extends BaseAdapter {
